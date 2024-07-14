6a42e223de817a34fb0791e15d01c036600c63e5
Subin <wien200922@gmail.com>
2024-07-14 21:52:53 +0900

    scrollTrigger test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import MainApp from './App/Main/MainApp';
 
@@ -7,6 +9,9 @@ import './App.css';
 import './index.css';
 import Header from './Header';
 import Footer from './Footer';
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 const App = () => {
   return (
