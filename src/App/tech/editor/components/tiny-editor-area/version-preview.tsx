09a4f0d7005f4d1cc8021bfaa1bdeff83665935b
유수빈 <wien2009@say>
2025-06-19 17:43:26 +0900

    fix: 버전 미리보기 css 수정

--- src/App/tech/editor/components/tiny-editor-area/version-preview.tsx
+++ src/App/tech/editor/components/tiny-editor-area/version-preview.tsx
@@ -6,11 +6,15 @@ type VersionPreviewProps = {
 
 export function VersionPreview({ version }: VersionPreviewProps) {
   return (
-    <div className="w-full pr-4 ">
+    <div className="w-full pr-4">
       <div className="font-bold mb-2 text-2xl">버전 관리</div>
-      <div className="bg-gray-100 p-2 h-[30rem] overflow-y-auto rounded-lg ">
-        {version && (
+      <div className="bg-gray-100 p-5 h-[30rem] overflow-y-auto rounded-lg ">
+        {version ? (
           <div dangerouslySetInnerHTML={{ __html: version.content }} />
+        ) : (
+          <div className="text-gray-400 font-medium text-center justify-center items-center h-full flex">
+            버전을 선택해주세요{" "}
+          </div>
         )}
       </div>
     </div>
